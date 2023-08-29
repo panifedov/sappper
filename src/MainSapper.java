79ba5d0a96c4d17a227f979bf455eb437a1cf203
Ivan Panifedov <ivan.panifedov@gmail.com>
2023-08-29 22:58:10 +0300

    add png/frst panel

--- src/MainSapper.java
+++ src/MainSapper.java
@@ -1,7 +1,6 @@
-package src.main.java;
-
 import javax.swing.*;
 import java.awt.*;
+import sweeper.Box;
 
 public class MainSapper extends JFrame
 {
@@ -18,12 +17,21 @@ public class MainSapper extends JFrame
     private MainSapper ()
     {
         initPanel();
+        setImages();
         initFrame();
     }
 
     private void initPanel()
     {
-        panel = new JPanel();
+        panel = new JPanel()
+        {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                for (Box box : Box.values())
+                    g.drawImage((Image)box.image, box.ordinal() * IMAGE_SIZE, 0,  this);
+            }
+        };
         panel.setPreferredSize(new Dimension(COLS*IMAGE_SIZE, IMAGE_SIZE*ROWS));
         add(panel);
     }
@@ -37,4 +45,18 @@ public class MainSapper extends JFrame
         setResizable(false);
         setVisible(true);
     }
+
+    private void setImages()
+    {
+        for (Box box : Box.values())
+            box.image = getImage(box.name().toLowerCase());
+    }
+
+    private Image getImage(String name)
+    {
+        String filename = "/res/img/" + name + ".png";
+        ImageIcon icon = new ImageIcon(getClass().getResource(filename));
+        return icon.getImage();
+    }
+
 }
---
+++ src/sweeper/Box.java
@@ -0,0 +1,21 @@
+package sweeper;
+import java.awt.Image;
+
+public enum Box {
+    ZERO,
+    NUM1,
+    NUM2,
+    NUM3,
+    NUM4,
+    NUM5,
+    NUM6,
+    NUM7,
+    NUM8,
+    BOMB,
+    OPEN,
+    FLAGED,
+    BOMBED,
+    NOBOMB;
+
+    public Image image; // Use the Image type
+}
